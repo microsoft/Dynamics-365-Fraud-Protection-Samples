35d676fa0251141b646a71b895bdc515ecff4500
Chad Sowald <chsowald@microsoft.com>
2020-08-03 17:20:59 -0700

    Switch over to MSAL from ADAL for getting tokens.

--- src/Infrastructure/Services/FraudProtectionSettings.cs
+++ src/Infrastructure/Services/FraudProtectionSettings.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+using System;
+
 namespace Contoso.FraudProtection.Infrastructure.Services
 {
     #region Fraud Protection Service
@@ -30,7 +32,7 @@ namespace Contoso.FraudProtection.Infrastructure.Services
 
     public class TokenProviderServiceSettings
     {
-        public string Resource { get; set; }
+        public Uri Resource { get; set; }
         public string ClientId { get; set; }
         public string Authority { get; set; }
         public string CertificateThumbprint { get; set; }
