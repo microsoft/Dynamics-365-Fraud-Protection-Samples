e2cd414ff9a7ffd779bbd69aae96c992d6fb70f7
Chad Sowald <chsowald@microsoft.com>
2020-12-08 15:06:45 -0800

    Remove unused import.

--- src/ApplicationCore/Entities/FraudProtectionApiModels/Response/Enrichments.cs
+++ src/ApplicationCore/Entities/FraudProtectionApiModels/Response/Enrichments.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-using System;
-
 namespace Contoso.FraudProtection.ApplicationCore.Entities.FraudProtectionApiModels.Response
 {
     public class Enrichments
